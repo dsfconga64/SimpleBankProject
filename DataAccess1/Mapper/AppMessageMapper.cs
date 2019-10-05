09a83034a14f1063d23e92b1e8cd2c2d08eed4fa
dsolanof <dsolanof@ucenfotec.ac.cr>
2019-10-05 10:40:42 -0600

    Added multiple changes on the exceptions layer

---
+++ DataAccess1/Mapper/AppMessageMapper.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess1.Dao;
+using Entities_POJO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess1.Mapper
+{
+    class AppMessageMapper : EntityMapper, ISqlStatements, IObjectMapper
+    {
+        private const string DB_COL_ID = "ID";
+        private const string DB_COL_TEXT = "TEXT";
+
+        public SqlOperation GetCreateStatement(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetDeleteStatement(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetRetriveAllStatement()
+        {
+            var operation = new SqlOperation { ProcedureName = "RET_ALL_MESSAGE_PR" };
+            return operation;
+        }
+
+        public SqlOperation GetRetriveStatement(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetUpdateStatement(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BaseEntity> BuildObjects(List<Dictionary<string, object>> lstRows)
+        {
+            var lstResults = new List<BaseEntity>();
+
+            foreach (var row in lstRows)
+            {
+                var appMessage = BuildObject(row);
+                lstResults.Add(appMessage);
+            }
+
+            return lstResults;
+        }
+
+        public BaseEntity BuildObject(Dictionary<string, object> row)
+        {
+            var appMessage = new ApplicationMessage
+            {
+                Id = GetIntValue(row, DB_COL_ID),
+                Message = GetStringValue(row, DB_COL_TEXT)
+            };
+
+            return appMessage;
+
+        }
+    }
+}
